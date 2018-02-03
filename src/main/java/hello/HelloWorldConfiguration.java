abd908b31ca8c253aebd63e037a7e8feb7938b04
miko <mk.dev@outlook.com>
2018-02-03 21:33:29 +0100

    First commit.

---
+++ src/main/java/hello/HelloWorldConfiguration.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello World Application for CodePipeline
+ */
+@SpringBootApplication
+public class HelloWorldConfiguration {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldConfiguration.class, args);
+	}
+
+}
